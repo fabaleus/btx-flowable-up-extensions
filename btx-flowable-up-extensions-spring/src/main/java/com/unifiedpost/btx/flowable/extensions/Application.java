407ad230edeb763f5898fa2f4d5e15b7765bd234
sjanssen <stef.janssens@unifiedpost.com>
2023-09-26 15:46:30 +0200

    BTX-0 Clean up for continuing flowable skeleton guide

--- btx-flowable-up-extensions-spring/src/main/java/com/unifiedpost/btx/flowable/extensions/Application.java
+++ btx-flowable-up-extensions-spring/src/main/java/com/unifiedpost/btx/flowable/extensions/Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 
-@SpringBootApplication(
-		scanBasePackages = {"com.unifiedpost.btx.flowable.extensions.*", "com.unifiedpost.btx.shared.monitoring"},
-		exclude = {FreeMarkerAutoConfiguration.class})
+@SpringBootApplication(exclude = {FreeMarkerAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {

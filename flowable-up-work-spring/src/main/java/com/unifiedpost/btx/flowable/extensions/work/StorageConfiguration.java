78341ede458e6399708c71e59730ec802fc105ea
Hans Boone <hans.boone@unifiedpost.com>
2023-10-10 16:05:23 +0200

    BTX-456 update of contentstorageconfig (not yet working)

--- flowable-up-work-spring/src/main/java/com/unifiedpost/btx/flowable/extensions/work/StorageConfiguration.java
+++ flowable-up-work-spring/src/main/java/com/unifiedpost/btx/flowable/extensions/work/StorageConfiguration.java
@@ -2,15 +2,23 @@
 package com.unifiedpost.btx.flowable.extensions.work;
 
 import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.unifiedpost.btx.flowable.extensions.storage.FlowableGcpCloudStorageProperties;
 import com.unifiedpost.btx.flowable.extensions.storage.GcpContentStorage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StorageConfiguration {
+	private FlowableGcpCloudStorageProperties properties;
 
 	@Bean
 	public GcpContentStorage gcpContentStorage(Storage storage) {
-		return new GcpContentStorage(storage, "");
+		return new GcpContentStorage(storage, properties.getBucket());
+	}
+
+	@Bean
+	public Storage gcpStorage() {
+		return StorageOptions.getDefaultInstance().getService();
 	}
 }
